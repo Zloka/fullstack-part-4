8873979ef70a50cb8d1bc507d0e2804ef71b8db3
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-24 09:30:24 +0200

    Save both users and blogs when creating a new blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const { author, url, likes, title } = request.body
-
   if (!title || !url) {
     return response.status(400).json({
       error: 'Both the title and url must be provided!'
     })
   }
+
+  const users = await User.find({}) // Assumes at least one user exists!
+  const user = users[0]
+
   const blog = new Blog({
-    author, url, title, likes: likes || 0
+    author, url, title, likes: likes || 0, user: user._id
   })
   const result = await blog.save()
+
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
   response.status(201).json(result)
 })
 
