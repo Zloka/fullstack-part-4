7eae17ce89d0ede09676210c47ef41a43a21ef9f
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-24 09:30:12 +0200

    Add blogs to the user model

--- models/user.js
+++ models/user.js
@@ -8,7 +8,14 @@ const userSchema = new mongoose.Schema({
   },
   name: String,
   passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ],
 })
+
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
